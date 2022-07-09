72e28bd4d06ca696d898442bad02de899bd8f055
Jesse Ilescas <ilesmj9516@gmail.com>
2022-07-08 21:05:54 -0500

    Entrega desafio 1

--- desafio1/desafioclases.js
+++ desafio1/desafioclases.js
@@ -1,1 +1,60 @@
-console.log('Hola')
+/* ---------------------------------- Clase --------------------------------- */
+class Usuario {
+    constructor (nombre, apellido, libros, mascotas) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.libros = libros
+        this.mascotas = mascotas
+    }
+    getFullName(){
+        console.log(`${nombre} ${apellido}`)
+    }
+    addMascota(mascota){
+        this.mascotas.push(mascota)
+    }
+    countMascotas(){
+        console.log('Numero de Mascotas:',mascotas.length);
+    }
+    addBook(nuevoLib){
+        this.libros.push(nuevoLib)
+    }
+    getBookNames(){
+        let bookNames = []
+        bookNames = libros.map(function (libro) {
+            let returnLibro = libro.nombre
+            return returnLibro
+        })
+        console.log(bookNames);
+    }
+}
+/* -------------------------------- Variables ------------------------------- */
+let nombre = 'Jesse'
+let apellido = 'Ilescas'
+let libros = [
+    {
+        nombre: "Harry Potter la Piedra Filosofal",
+        autor:  "J. K. Rowling"
+    },
+    {
+        nombre: "Dune",
+        autor : "Frank Herbert"
+    },
+    {
+        nombre: "Neuromante",
+        autor: "William Gibson"
+    }
+]
+let mascotas = ['Perro', 'Gato', 'Pez']
+const nuevoLibro = {
+    nombre: 'El problema de los tres cuerpos',
+    autor: 'Liu Cixin'
+}
+/* ---------------------------- Instanciar Clase ---------------------------- */
+const usuario = new Usuario(nombre, apellido, libros, mascotas)
+
+/* ---------------------------- Llamada a métodos --------------------------- */
+usuario.getFullName()
+usuario.addMascota('Loro')
+usuario.countMascotas()
+usuario.addBook(nuevoLibro)
+usuario.getBookNames()
